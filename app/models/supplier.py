e5528cf13ab2f2b0dea7dd33adefca30789ae2be
Chacha Chacha <calex2607@gmail.com>
2024-12-19 19:29:16 +0300

    Update supplier.py

--- app/models/supplier.py
+++ app/models/supplier.py
@@ -5,8 +5,10 @@ from . import BaseModel
 class Supplier(BaseModel):
     __tablename__ = 'suppliers'
 
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    phone_number = db.Column(db.String(50), nullable=False, unique=True, index=True)
     contact_info = db.Column(db.JSON, nullable=True)
+    tax_id = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return f"<Supplier {self.name}>"
