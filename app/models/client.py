3d08baad226a92dff657b6029f95a42617a552a3
Chacha Chacha <calex2607@gmail.com>
2024-12-22 21:24:28 +0300

    Update client.py

--- app/models/client.py
+++ app/models/client.py
@@ -12,5 +12,14 @@ class Client(BaseModel):
     contact_info = db.Column(db.JSON, nullable=True)
     tax_id = db.Column(db.String(50), nullable=True)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone_number': self.phone_number,
+            'contact_info': self.contact_info,
+            'tax_id': self.tax_id
+        }
+
     def __repr__(self):
         return f"<Client {self.name} - {self.phone_number}>"
